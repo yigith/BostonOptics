107ed8c491a390718ee6473d9e150ebdf679781c
yigith <yigith1@gmail.com>
2023-02-17 17:42:33 +0300

    dbcontext

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 A Sample N-layered .NET Core Project Demonstrating Clean Architecture and the Generic Repository Pattern.
 
 ## Packages Installed
+
+### Infrastructure
+```
+Install-Package Microsoft.EntityFrameworkCore -v 6.0.14
+Install-Package Microsoft.EntityFrameworkCore.Tools -v 6.0.14
+Install-Package Npgsql.EntityFrameworkCore.PostgreSQL -v 6.0.8
+```
+
 ### ApplicationCore
 ```
 Install-Package Ardalis.Specification -v 6.1.0
---
+++ src/Infrastructure/Data/ShopContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Brand> Brands => Set<Brand>();
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // mevcut projedeki (Infrastructure) entity ayar dosyalarını bul ve uygula
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
