483ce01f5413d7c5c1ad006a0fc013ed0fe27482
yigith <yigith1@gmail.com>
2023-02-21 16:26:24 +0300

    identity seed

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -51,7 +51,11 @@ app.MapRazorPages();
 using (var scope = app.Services.CreateScope())
 {
     var shopContext = scope.ServiceProvider.GetRequiredService<ShopContext>();
+    var identityContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
     await ShopContextSeed.SeedAsync(shopContext);
+    await AppIdentityDbContextSeed.SeedAsync(identityContext, roleManager, userManager);
 }
 
 app.Run();
