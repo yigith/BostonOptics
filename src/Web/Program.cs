f8701893b2f1ef4d61fc23b90d4382ced8372a65
yigith <yigith1@gmail.com>
2023-02-21 14:50:02 +0300

    repo test

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -2,6 +2,8 @@ global using Infrastructure.Identity;
 global using Infrastructure.Data;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
+global using ApplicationCore.Interfaces;
+global using ApplicationCore.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,7 @@ builder.Services.AddDbContext<AppIdentityDbContext>(options =>
 builder.Services.AddDbContext<ShopContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("ShopContext")));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EFRepository<>));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
